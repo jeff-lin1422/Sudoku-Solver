51b5fe1af45c556f3d33ec70a60d44d1400497f8
meowfish <linjeff16@gmail.com>
2021-02-10 14:39:08 -0500

    added solution function, comment on how code works

--- sudoku_solver/solver.py
+++ sudoku_solver/solver.py
@@ -31,7 +31,7 @@ def findEmpty(board):
                 return (i,j)
     return False
 
-
+#check if the input is valid
 def check(board, number, position):
     #check row
     #goes through the row and check for number
@@ -42,17 +42,42 @@ def check(board, number, position):
     #check column
     #goes through the column and check for number
     #if the column contains the number that's not in the empty space position, return false
-    for i in range(len(board[0])):
     for i in range(len(board)):
         if board[i][position[1]] == number and position[0] != i:
             return False
     #check the whole box
+    #goes through the box, if you find the number, then you check if the position
+    #is the same as the empty box position
+    #return false if it's not, else return true
     box_row = position[1]//3
     box_column = position[0]//3
-    for x in range(box_row, box_row + 3):
-        for y in range(box_column, box_column + 3):
-            if board[x][y] == number and (x,y) != position:
+    for y in range(box_column*3, box_column*3 + 3):
+        for x in range(box_row*3, box_row*3 + 3):
+            if board[y][x] == number and (y,x) != position:
                 return False
     return True
 
+#solves the sudoku
+def solution(board):
+    #gets the coordinates
+    found = findEmpty(board)
+    if not found:
+        return True
+    else:
+        row, column = found
+    #go through all numbers on each space
+    for i in range(1,10):
+        if check(board, i, (row, column)):
+            board[row][column] = i
+
+            if solution(board):
+                return True
+
+            board[row][column] = 0
+    return False
+
+
+printBoard(board)
+solution(board)
+print("*********************************")
 printBoard(board)
